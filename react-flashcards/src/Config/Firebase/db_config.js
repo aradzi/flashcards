955997ffd170b79a989dff07f1163abd54ce72f7
Ana Radzi <anaradzi@hotmail.com>
2019-01-27 00:56:47 -0800

    finished app, w some problems

---
+++ react-flashcards/src/Config/Firebase/db_config.js
@@ -0,0 +1,9 @@
+export const DB_CONFIG = {
+
+    apiKey: "AIzaSyCeb-1OiWVG_bFoUKWBexPCLwtLkg7L0MM",
+    authDomain: "react-flashcards-9bc99.firebaseapp.com",
+    databaseURL: "https://react-flashcards-9bc99.firebaseio.com",
+    projectId: "react-flashcards-9bc99",
+    storageBucket: "react-flashcards-9bc99.appspot.com",
+    messagingSenderId: "818914618401"
+}
